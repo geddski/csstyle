4e4ae0a3d2aaacc309aa36be224cea8636f7e7df
Dave Geddes <davidcgeddes@gmail.com>
2014-09-22 21:59:58 -0600

    (tests) prep setup for postcss support

--- spec/csstyle-spec.js
+++ spec/csstyle-spec.js
@@ -1,24 +1,39 @@
 describe("component", function(){
   it("creates a class", function(){
-    var selector = getSelector('spec/scss/fixtures/component.css');      
-    expect(selector.value).toBe('.tweet');
-    expect(selector.score).toBe('0,0,1,0');
+    [
+      getSelector('spec/scss/fixtures/component.css'),
+      getSelector('spec/postcss/fixtures/component.css')
+    ]
+    .forEach(function(selector){
+      expect(selector.value).toBe('.tweet');
+      expect(selector.score).toBe('0,0,1,0');
+    })
   });
 });
 
 describe("option", function(){
   it("appends a --class to the component", function(){
-    var selector = getSelector('spec/scss/fixtures/option.css', 1);
-    expect(selector.value).toBe(".tweet.\\--promoted");
-    expect(selector.score).toBe('0,0,2,0');
+    [
+      getSelector('spec/scss/fixtures/option.css', 1)
+      // getSelector('spec/postcss/fixtures/option.css', 1)      
+    ]
+    .forEach(function(selector){
+      expect(selector.value).toBe(".tweet.\\--promoted");
+      expect(selector.score).toBe('0,0,2,0');
+    })
   });
 });
 
 describe("part", function(){
   it("appends a __class to the component", function(){
-    var selector = getSelector('spec/scss/fixtures/part.css', 1);
-    expect(selector.value).toBe(".tweet__image");
-    expect(selector.score).toBe('0,0,1,0');
+    [
+      getSelector('spec/scss/fixtures/part.css', 1)
+      // getSelector('spec/postcss/fixtures/part.css', 1)      
+    ]
+    .forEach(function(selector){
+      expect(selector.value).toBe(".tweet__image");
+      expect(selector.score).toBe('0,0,1,0');
+    })
   });
 
   describe("parts nested in parts", function(){
@@ -62,17 +77,27 @@ describe("part", function(){
 
 describe("tweak", function(){
   it("defines a +class after csstyle id", function(){
-    var selector = getSelector('spec/scss/fixtures/tweak.css');
-    expect(selector.value).toBe("#csstyle .\\+rounded");
-    // waiting on https://github.com/keeganstreet/specificity/issues/10
-    // expect(selector.score).toBe('0,1,1,0');
+    [
+      getSelector('spec/scss/fixtures/tweak.css')
+      // getSelector('spec/postcss/fixtures/tweak.css')      
+    ]
+    .forEach(function(selector){
+      // waiting on https://github.com/keeganstreet/specificity/issues/10
+      // expect(selector.score).toBe('0,1,1,0');
+      expect(selector.value).toBe("#csstyle .\\+rounded");
+    })
   });
 });
 
 describe("location", function(){
   it("defines a @class after csstyle id", function(){
-    var selector = getSelector('spec/scss/fixtures/location.css');
-    expect(selector.value).toBe("#csstyle .\\@home");
-    expect(selector.score).toBe('0,1,1,0');
+    [
+      getSelector('spec/scss/fixtures/location.css')
+      // getSelector('spec/postcss/fixtures/location.css')      
+    ]
+    .forEach(function(selector){
+      expect(selector.value).toBe("#csstyle .\\@home");
+      expect(selector.score).toBe('0,1,1,0');
+    })
   });
 });
--- spec/setup-spec.js
+++ spec/setup-spec.js
@@ -1,10 +1,22 @@
 require('shelljs/global');
 var specificity = require('specificity');
+var postcss = require('postcss');
+var fs = require('fs-extra');
+var glob = require("glob");
 
 // delete previous css
 exec('rm -rf spec/scss/fixtures/*.css');
-// compile fixtures
+
+// compile sass fixtures
 exec('sass --update spec/scss/fixtures --style expanded --sourcemap=none');
+
+// compile postcss fixtures
+var processor = postcss([require('postcss-nested')]);
+var files = glob.sync(__dirname + "/postcss/fixtures/**/*.pcss");
+files.forEach(function(file){
+  var result = processor.process(fs.readFileSync(file));
+  fs.writeFileSync(file.replace('.pcss', '.css'), result.css);
+});
 
 var customMatchers = {
   toBeMoreSpecificThan: function(util, customEqualityTesters) {
