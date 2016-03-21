0041d0acdee9582750bb22afef2512f1b1df6a86
Anthony Nichols <anichols@curse.com>
2016-03-21 13:59:33 -0700

    Update selector-helper regex for matching selectors for Stylus support
    The regex needed to match multi-line comma-delimited selectors in
    addition to space-delimited (how Sass and PostCSS compile).
    e.g.:
    ```
    .frame:hover .frame__img,
    .frame:focus .frame__img {
      ...
    }
    ```
    as well as
    ```
    .frame:hover .frame__img, .frame:focus .frame__img {
     ...
    }
    ```

--- spec/selector-helper.js
+++ spec/selector-helper.js
@@ -2,14 +2,14 @@ var specificity = require('specificity');
 var fs = require('fs');
 var path = require('path');
 
-module.exports = { 
+module.exports = {
   /**
    * grab a selector from a file and calculate its specificity
    */
   getSelector: function(file, number){
     number = number || 0;
     var contents = fs.readFileSync(path.resolve(file)).toString();
-    var selector = contents.match(/.*\{/g)[number].split('{')[0].trim();
+    var selector = contents.match(/[^\}]*\{/g)[number].split('{')[0].trim();
     var score = specificity.calculate(selector)[0].specificity;
 
     return {
