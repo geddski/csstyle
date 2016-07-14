fbfeb9bd91fa3a38d1314dfa48c0cb46980011b9
Sven Wittevrongel <cupoftea696@gmail.com>
2016-07-14 18:28:20 +0100

    more location tests

--- tests/scss.browser.js
+++ tests/scss.browser.js
@@ -1,7 +1,7 @@
 describe('csstyle', function() {
   var frame;
   var app;
-  var test;
+  var component, part;
   var i = 1;
   
   before(function(done) {
@@ -20,39 +20,60 @@ describe('csstyle', function() {
     app = frame.add(
       '<div id="app">' +
         '<div class="@tst' + i + '-l tst' + i + '-c2">' +
-          '<div class="tst' + i + '-c tst' + i + '-c1 --tst' + i + '-o" id="test">Test</div>' +
+          '<div class="tst' + i + '-c tst' + i + '-c1 --tst' + i + '-o +tst' + i + '-t" id="component">' +
+            '<div class="tst' + i + '-c/tst' + i + '-p" id="part">test</div>' +
+          '</div>' +
         '</div>' +
       '</div>',
       'location'
     );
     
-    test = frame.get('#test');
+    component = frame.get('#component');
+    part = frame.get('#part');
     i++;
   });
   
   describe('Locations', function() {
+    it('should overwrite components', function() {
+      component.assert({
+        width: 2
+      });
+    });
+    
+    it('should overwrite parts', function() {
+      part.assert({
+        width: 2
+      });
+    });
+    
     it('should not overwrite options', function() {
-      test.assert({
+      component.assert({
         width: 2
       });
     });
     
     it('should overwrite options inside the location', function() {
-      test.assert({
+      component.assert({
         width: 4
+      });
+    });
+    
+    it('should overwrite tweaks', function() {
+      component.assert({
+        width: 2
       });
     });
   });
   
   describe('Nested Components', function() {
     it('should behave like locations and not overwrite options', function() {
-      test.assert({
+      component.assert({
         width: 2
       });
     });
     
     it('should behave like locations and overwrite options inside the nested component', function() {
-      test.assert({
+      component.assert({
         width: 4
       });
     });
